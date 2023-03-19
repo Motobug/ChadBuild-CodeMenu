c4911cb0a0a3f550234e0c9936feed44eabfe25d
Paul Joseph Canavan <ssbmotobug@gmail.com>
2023-03-19 15:36:48 -0400

    New Chadbuild Code Menu

--- PowerPC Assembly Functions/Code Menu.cpp
+++ PowerPC Assembly Functions/Code Menu.cpp
@@ -291,9 +291,9 @@ void CodeMenu()
 	
 	MainLines.push_back(&DebugMode.CalledFromLine);
 #if DOLPHIN_BUILD
-	MainLines.push_back(new Selection("Stagelist", { "PMBR", "Theatre", "Middle 3", "Motolist", "NY Stages" }, 0, STAGELIST_INDEX));
+	MainLines.push_back(new Selection("Stagelist", { "PMBR", "Middle 3", "2023 Proposed", "Ontario", "NUTS", "Australia", "All"}, 1, STAGELIST_INDEX));
 #else
-	MainLines.push_back(new Selection("Stagelist", { "PMBR", "Theatre", "Middle 3", "Motolist", "NY Stages" }, 0, STAGELIST_INDEX));
+	MainLines.push_back(new Selection("Stagelist", { "PMBR", "Middle 3", "2023 Proposed", "Ontario", "NUTS", "Australia", "All"}, 1, STAGELIST_INDEX));
 #endif
 	constantOverrides.emplace_back(0x80523400, STAGELIST_INDEX);
 	//	MainLines.push_back(new Selection("Endless Friendlies", { "OFF", "Same Stage", "Random Stage", "Round Robin" }, 0, INFINITE_FRIENDLIES_INDEX));
